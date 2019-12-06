ab8ad91ab23e24825749f70a8cc2c5d14885eb76
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2019-12-06 01:58:17 +0100

    a lots a change and i am doind different days The code is dirty i should clean it

--- day01/Module.py
+++ day01/Module.py
@@ -2,18 +2,42 @@
 # @Author: Baptiste Bertrand-Rapello
 # @Date:   2019-12-01 22:43:24
 # @Last Modified by:   Baptiste Bertrand-Rapello
-# @Last Modified time: 2019-12-01 23:51:44
+# @Last Modified time: 2019-12-03 13:04:25
 
 import math
+from WireMod import * 
+
+def calc_fuel_require(ms):
+	intPart = math.modf(ms / 3)
+	fl = int(intPart[1]) - 2
+	return fl
 
 class Module:
 	def __init__(self, ms):
 		self.fuel = 0
 		self.masse = ms
+		self.fuelForFuel = 0
+		self.totalFuel = 0
+		self.wm = WireMod()
+
+	def computeFuelForFuel(self):
+		rtr = calc_fuel_require(self.fuel)
+		self.fuelForFuel = rtr
+		#print("we need : ", self.fuelForFuel, " for the fuel : ", self.fuel)
+		while rtr > 2:
+			prevrtr = rtr
+			rtr = calc_fuel_require(rtr)
+			self.fuelForFuel = self.fuelForFuel + rtr
+			#print("we need : ", rtr, " for the fuel : ", prevrtr, " and we have : ", self.fuelForFuel)
 
 	def computeFuel(self):
-		intPart = math.modf(self.masse / 3)
-		self.fuel = int(intPart[1]) - 2
+		self.fuel = calc_fuel_require(self.masse)
+		# self.computeFuelForFuel()
+		self.totalFuel = self.fuel + self.fuelForFuel
+
 
 	def getFuel(self):
-		return self.fuel
+		return self.totalFuel
+
+	def initWireModule(self):
+		self.wm.initPan()
--- day01/main.py
+++ day01/main.py
@@ -2,12 +2,15 @@
 # @Author: Baptiste
 # @Date:   2019-12-01 22:18:06
 # @Last Modified by:   Baptiste Bertrand-Rapello
-# @Last Modified time: 2019-12-02 00:13:47
+# @Last Modified time: 2019-12-06 00:31:35
 
 #!/usr/local/bin/python3
 
 import math
 from Module import * 
+from Intcode import *
+from PasswordBreaker import *
+import time
 
 def open_a_file(file_name):
 	try:
@@ -58,6 +61,13 @@ def sum_of_everything(lst):
 		#print("res provisoir = ", res)
 	print("--> Sum of everything : ", res)
 
+def test_intcode_computer(lst):
+	computer = Intcode()
+	#print("###> dans test du int code et la list est : ", lst)
+	lstRes = computer.computeIntcode(lst)
+	#print("###> et la liste est mainteannt : ", lst)
+	return lstRes
+
 def main():
 	print("In The Main function")
 	modTwelve = Module(12)
@@ -96,6 +106,8 @@ def main():
 	else:
 		print("\033[92mOK!\033[0m : for 100756 it is 33583 the test passed ;-p")
 
+	modHundredThousandSevenFiveSix.computeFuelForFuel()
+
 	test_computation(100756, 33583)
 	
 	# modTest = Module(119580)
@@ -106,6 +118,45 @@ def main():
 	test_computation(37789, 12594)
 	test_computation(26751, 8915)
 	test_computation(33745, 11246)
+	#test_intcode_computer([1,9,10,3,2,3,11,0,99,30,40,50])
+	#test_intcode_computer([1,0,0,0,99])
+	#test_intcode_computer([2,3,0,3,99])
+	#test_intcode_computer([2,4,4,5,99,0])
+	#test_intcode_computer([1,1,1,4,99,5,6,0,99])
+	theList = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,9,1,19,1,19,5,23,1,23,6,27,2,9,27,31,1,5,31,35,1,35,10,39,1,39,10,43,2,43,9,47,1,6,47,51,2,51,6,55,1,5,55,59,2,59,10,63,1,9,63,67,1,9,67,71,2,71,6,75,1,5,75,79,1,5,79,83,1,9,83,87,2,87,10,91,2,10,91,95,1,95,9,99,2,99,9,103,2,10,103,107,2,9,107,111,1,111,5,115,1,115,2,119,1,119,6,0,99,2,0,14,0]
+	size = len(theList)
+	print("la liste est d'une taille : ", size, "et est :", theList)
+	#test_intcode_computer()
+	i = 0
+	j = 0
+	while j < 100:
+		while i < 100:
+			#print("i = ", i, " et j = ", j)
+			#print(theList)
+			resLst = test_intcode_computer(theList)
+			#print("id the list = ", id(theList), "id lst : ", id(resLst))
+			#print(resLst)
+			#print(resLst[0])
+			#if i > 3:
+			#	resLst[0] = 19690720
+			#	print("bis : ", resLst[0] )
+			if resLst[0] == 19690720: 
+				print("le resultat est trouvé")
+				print(resLst[1], "  ", resLst[2])
+				print(resLst)
+				break
+			else :
+				i += 1
+				theList[1] = i
+			#time.sleep(1)
+
+		j += 1
+		theList[2] = j
+		i = 0
+		theList[1] = i
+
+
+	#test_intcode_computer([1,12,2,0,99, ])
 
 def test_computation(ms, resGoaled):
 	modTest = Module(ms)
@@ -117,4 +168,11 @@ def test_computation(ms, resGoaled):
 		print("\033[92mOK!\033[0m : for", ms, "it is", resGoaled, "the test passed ;-p")
 
 
-main()
+def mainDeux():
+	pb = PassBrkr("273025-767253")
+	#pb = PassBrkr("123456-444444")
+	pb.breakeIt()
+
+
+# main()
+mainDeux()
